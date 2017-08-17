b44caadcfb920bb32cbb4af9977a0be92f89b42f
Rameshwar Bhaskaran <rameshwar.zorro@gmail.com>
2017-08-17 19:21:00 +0530

    Added a basic structure. Model still left to be done.

--- hangman.py
+++ hangman.py
@@ -1,2 +1,30 @@
+import editdistance
+
 def model(maskedWord, guesses):
     # create model here
+    return predictedCharacter
+
+def hangman(solution):
+    guesses = {}
+    wrongGuess = 0
+    maskedWord = "_"*len(solution)
+    while wrongGuess < 8 and '_' in maskedWord:
+        predictedCharacter = model(maskedWord, guesses)
+        if predictedCharacter in guesses:
+            continue
+        guesses[predictedCharacter] = 1
+        if predictedCharacter in solution:
+            for i in range(0, len(solution)):
+                if solution[i] == predictedCharacter:
+                    maskedWord[i] = predictedCharacter
+        else:
+            wrongGuess += 1
+
+    return maskedWord, guesses
+
+def loss(solution, prediction):
+    # find the levenshtein distance between the two words
+    return editdistance(solution, prediction)
+
+
+
