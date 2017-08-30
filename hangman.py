434dc73e4e32f8a46ce140ca2531daa8a040514f
Rameshwar Bhaskaran <rameshwar.zorro@gmail.com>
2017-08-30 18:10:01 +0530

    TRigram 1.65

--- hangman.py
+++ hangman.py
@@ -29,29 +29,43 @@ def train_ngrams():
     count = 0
     f = open("train.txt")
     bigram_model = {}
+    trigram_model = {}
     for line in f:
         count+=1
         word = line.strip().split()[0]
         if len(word) not in bigram_model:
             bigram_model[len(word)] = {}
+            trigram_model[len(word)] = {}
         chars = [c for c in word]
         bigrams = ngrams(chars,2)
+        trigrams = ngrams(chars, 3)
         for bigram in bigrams:
             if bigram[0] not in bigram_model[len(word)]:
                 bigram_model[len(word)][bigram[0]] = {}
             if bigram[1] not in bigram_model[len(word)][bigram[0]]:
-                bigram_model[len(word)][bigram[0]][bigram[1]] = 0
-            bigram_model[len(word)][bigram[0]][bigram[1]] += 1
+                bigram_model[len(word)][bigram[0]][bigram[1]] = 1
+            else:
+                bigram_model[len(word)][bigram[0]][bigram[1]] += 1
+        for trigram in trigrams:
+            a,b,c = trigram
+            if (a,b) not in trigram_model[len(word)]:
+                trigram_model[len(word)][(a,b)] = {}
+            if c not in trigram_model[len(word)][(a,b)]:
+                trigram_model[len(word)][(a,b)][c] = 1
+            else:
+                trigram_model[len(word)][(a,b)][c] += 1
     f = open("bigram_model.pkl","wb")
-    print count
     pickle.dump(bigram_model, f, -1) 
+    f = open("trigram_model.pkl","wb")
+    pickle.dump(trigram_model, f, -1) 
+    print "Finished training"
     return bigram_model
 
-
-train_letterfrequency()
-train_ngrams()
+#train_letterfrequency()
+#train_ngrams()
 letter_freq_model = pickle.load(open("model.pkl"))
 trained_model = pickle.load(open('bigram_model.pkl'))
+trained_tri_model = pickle.load(open('trigram_model.pkl'))
 
  # return some letter not in guesses
 def getRandomLetter(guesses):
@@ -86,10 +100,16 @@ def model(maskedWord, guesses):
         if maskedWord[i] == '_':
             if i == 0 or maskedWord[i-1] == '_':
                 continue
-            for k,v in sorted(trained_model[size][maskedWord[i-1]].items(), key=operator.itemgetter(1), reverse=True):
-                if k not in guesses:
-                    return k
-                    break
+            if i>=2 and maskedWord[i-1]!='_' and maskedWord[i-2]!='_' and (maskedWord[i-2], maskedWord[i-1]) in trained_tri_model[size]:
+                for k,v in sorted(trained_tri_model[size][(maskedWord[i-2], maskedWord[i-1])].items(), key=operator.itemgetter(1), reverse=True):
+                    if k not in guesses:
+                        return k
+                        break
+            else:
+                for k,v in sorted(trained_model[size][maskedWord[i-1]].items(), key=operator.itemgetter(1), reverse=True):
+                    if k not in guesses:
+                        return k
+                        break
             break
     
     if maskedWord[i] == '_':
